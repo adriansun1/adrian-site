dcdfb67a821f29457511e2912e1bee4384febfda
Adrian Sun <adriansun2@gmail.com>
2020-12-28 23:15:02 -0800

    create a work and personal persona

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,7 +10,8 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import "./layout.css"
+import "normalize.css";
+import GlobalStyles from '../style/GlobalStyles';
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -25,7 +26,7 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
+          <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
       <div
         style={{
           margin: `0 auto`,
@@ -33,6 +34,7 @@ const Layout = ({ children }) => {
           padding: `0 1.0875rem 1.45rem`,
         }}
       >
+        <GlobalStyles/>
         <main>{children}</main>
         <footer style={{
           marginTop: `2rem`
---
+++ src/pages/personal.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const Personal = () => (
+  <Layout>
+    <SEO title="personal" />
+    <h1>Personal</h1>
+  </Layout>
+)
+
+export default Personal
