9f9a3da1a78a15ae380c921883e6aafa87937ba5
Adrian Sun <adriansun2@gmail.com>
2021-01-07 02:26:53 -0800

    Interest chart

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,25 +1,14 @@
 import Landing from '../components/Landing';
 import Navbar from '../components/Navbar';
-import styled from 'styled-components';
+import InterestChart from '../components/InterestChart';
 
-const CircleElement = styled.div`
-  height: 100vh;
-  h3{
-    padding:3rem;
-  }
-`;
 
 export default function Home() {
   return (
     <div>
       <Navbar isAbsolute />
       <Landing />
-      <CircleElement>
-          <h3>
-            This is my personal site, I use it to keep track the
-            stuff I'm interested in:
-          </h3>
-      </CircleElement>
+      <InterestChart/>
     </div>
   );
 }
