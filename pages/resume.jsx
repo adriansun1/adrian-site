8328b9682ece0f048639681345c1de4d17034493
Adrian Sun <adriansun2@gmail.com>
2021-07-19 01:51:22 -0700

    add cbg to resume for smoother transition

--- pages/resume.jsx
+++ pages/resume.jsx
@@ -1,34 +1,43 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
-import {FaFilePdf} from 'react-icons/fa';
+import { FaFilePdf } from 'react-icons/fa';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: #222831 ;
+  }
+`;
 
 export default function Resume() {
   const [site, setSite] = useState();
   const url = 'https://d13b2cruxwxefb.cloudfront.net/';
 
   const StyledDiv = styled.div`
-		.nav {
-			background-color:#ff5701;
-			width:100%;
-			height:10px;
-			position:absolute;
-		}
-    a {
-			display:flex;
-			align-items:center;
-			gap:3px;
-      position: absolute;
-      color: #eeeeee;
-      background-color: #ff5701;
-      top: 10px;
-      right: 18px;
-      padding: 2px 8px;
-      border-radius: 0 0 5px 5px;
-      font-size: 12px;
-			p{
-				margin:0;
-				font-weight: 400;
-			}
+    .navbar {
+      .nav {
+        background-color: #ff5701;
+        width: 100%;
+        height: 10px;
+        position: absolute;
+      }
+      a {
+        display: flex;
+        align-items: center;
+        gap: 3px;
+        position: absolute;
+        color: #eeeeee;
+        background-color: #ff5701;
+        top: 10px;
+        right: 18px;
+        padding: 2px 8px;
+        border-radius: 0 0 5px 5px;
+        font-size: 12px;
+        p {
+          margin: 0;
+          font-weight: 400;
+        }
+      }
     }
   `;
 
@@ -46,11 +55,17 @@ export default function Resume() {
   console.log(site);
   return (
     <>
+      <GlobalStyle />
       <StyledDiv>
-        <div className='nav'></div>
-        <a href={url + 'resume.pdf'}><FaFilePdf/><p>PDF</p></a>
+        <div className='navbar'>
+          <div className='nav' />
+          <a href={url + 'resume.pdf'}>
+            <FaFilePdf />
+            <p>PDF</p>
+          </a>
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: site }}></div>
       </StyledDiv>
-      <div dangerouslySetInnerHTML={{ __html: site }}></div>
     </>
   );
 }
