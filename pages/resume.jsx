b99a6b6692e27bb0b91962edbbbc21d21da816f3
Adrian Sun <adriansun2@gmail.com>
2021-07-19 01:25:33 -0700

    add navbar for smoother transition

--- pages/resume.jsx
+++ pages/resume.jsx
@@ -5,20 +5,27 @@ export default function Resume() {
   const [site, setSite] = useState();
   const url = 'https://d13b2cruxwxefb.cloudfront.net/';
 
-	const StyledDiv = styled.div`
-		position: absolute;
-		color: #EEEEEE;
-		background-color:#FF5701;
-		top: 10px;
-		right: 18px;
-		padding:2px 8px;
-		border-radius: 0 0 5px 5px;
-		font-size:10px;
-
-	`;
+  const StyledDiv = styled.div`
+		.nav {
+			background-color:#ff5701;
+			width:100%;
+			height:10px;
+			position:absolute;
+		}
+    a {
+      position: absolute;
+      color: #eeeeee;
+      background-color: #ff5701;
+      top: 10px;
+      right: 18px;
+      padding: 2px 8px;
+      border-radius: 0 0 5px 5px;
+      font-size: 10px;
+    }
+  `;
 
   async function fetchSite() {
-		//TODO get diff resume based on path
+    //TODO get diff resume based on path
     const res = await fetch(`${url}resume.html`);
     const data = await res.text();
     setSite(data);
@@ -31,9 +38,10 @@ export default function Resume() {
   console.log(site);
   return (
     <>
-			<StyledDiv>
-				<a href={url+'resume.pdf'}>PDF</a>
-			</StyledDiv>
+      <StyledDiv>
+        <div className='nav'></div>
+        <a href={url + 'resume.pdf'}>PDF</a>
+      </StyledDiv>
       <div dangerouslySetInnerHTML={{ __html: site }}></div>
     </>
   );
